6b51b34390aabf58ca70c4ca644fec19a1d4df81
mben-has <mben-has@student.42heilbronn.de>
2024-12-25 05:20:05 +0100

    profiles_und_features

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -11,8 +13,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username  = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect ('blog-home')
+            messages.success(request, f'Account created! You are able to login')
+            return redirect ('login')
     else :
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+def custom_logout(request):
+    logout(request)
+    return redirect('login') 
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
